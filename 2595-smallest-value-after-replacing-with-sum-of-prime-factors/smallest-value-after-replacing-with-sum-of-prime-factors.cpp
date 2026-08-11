82fa2e7d16bbb397d5f89ab2b162933a6b1cafd1
harishE-coder <harishabblu123@gmail.com>
2026-08-11 15:31:09 +0530

    Time: 2 ms (26.24%) | Memory: 8 MB (20.66%) - LeetSync

--- 2595-smallest-value-after-replacing-with-sum-of-prime-factors/smallest-value-after-replacing-with-sum-of-prime-factors.cpp
+++ 2595-smallest-value-after-replacing-with-sum-of-prime-factors/smallest-value-after-replacing-with-sum-of-prime-factors.cpp
@@ -1,38 +1,16 @@
 class Solution {
 public:
-    static const int Max = 1e6;
-    vector<int> res;
-
-
-    void spf() {
-        res.resize(Max + 1);
-        for (int i = 0; i <= Max; i++)
-            res[i] = i;
-        for (int i = 2; i * i <= Max; i++) {
-            if (res[i] == i) {
-                for (int j = i * i; j <= Max; j += i) {
-                    if (res[j] == j)
-                        res[j] = i;
+    int smallestValue(int n) {
+        while (true) {
+            int sum = 0, num = n, x = num;
+            for (int p = 2; p * p <= x; p++) {
+                while (x % p == 0) {
+                    sum += p;
+                    x /= p;
                 }
             }
-        }
-    }
-
-    int smallestValue(int n) {
-        spf();
-
-        while (true) {
-            int sum = 0;
-            int x = n;
-
-            while (x > 1) {
-                sum += res[x];
-                x /= res[x];
-            }
-
-            if (sum == n)
-                return n;
-
+            if (x > 1) sum += x;
+            if (sum == n) return n;
             n = sum;
         }
     }
